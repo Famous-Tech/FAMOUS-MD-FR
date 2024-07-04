aa87463d0674b5a777b06764702ea355db599d58
A7-T62 <109889758+Diegoson@users.noreply.github.com>
2024-07-04 13:56:47 +0200

    A7-T62

---
+++ contents/commands/ping.js
@@ -0,0 +1,18 @@
+const { defineCommand } = require('../commands.js'); 
+
+defineCommand({
+    pattern: 'ping',
+    category: 'mics',
+    handler: async (sock, msg) => {
+        
+        const start = new Date().getTime();
+        const edited = await sock.sendMessage(msg.chat, { text: '🏓 Pinging...' }, { quoted: msg });
+
+        const end = new Date().getTime();
+        const TripTime = end - start;
+        await sock.sendMessage(msg.chat, {
+            text: `🏓 Pong!: \`${TripTime} ms`,
+            edit: edited.key 
+        });
+    }
+});
