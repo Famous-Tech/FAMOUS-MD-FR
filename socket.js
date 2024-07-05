74dfe669be5526234631885faee267e52594989a
A7-T62 <109889758+Diegoson@users.noreply.github.com>
2024-07-05 19:42:42 +0200

    A7-T62

--- socket.js
+++ socket.js
@@ -11,8 +11,9 @@ const { imageSync } = require('qr-image');
 const contact = require('./lib/contact');
 const control = require('./lib/commands');
 const { serialize } = require('./lib/serialize.js');
-const { QuickDatabase, db,.mongoDB } = require('./contents/QuickDB/Database');
+const { QuickDatabase, mongoDB } = require('./contents/QuickDB/Database');
 const Authentication = require('./contents/QuickDB/Schema');
+const config = require('./config');
 const { state, saveCreds } = useMultiFileAuthState('./lib/auth_info_multi');
 const store = makeInMemoryStore({ logger: P().child({ level: 'silent', stream: 'store' }) });
 store.readFromFile('./lib/baileys_store_multi.json');
@@ -20,14 +21,13 @@ setInterval(() => {
     store.writeToFile('./lib/baileys_store_multi.json');
 }, 10000);
 
-async function waveWhatsApp() { 
-    const db = new QuickDatabase(config)
-    await mongoDB.connect()
-    const { DatabaseMulti } = new Authentication(config.session, db)
-    const SessionMulti= await DatabaseMulti()
-    new (config, SessionMulti,db, {
-        
-    fetchLatestWaWebVersion().then(({ version, isLatest }) => {
+async function WaSock() {
+    const db = new QuickDatabase(config);
+    await mongoDB.connect();
+    const DatabaseMulti = new Authentication(config.session, db);
+    const SessionMulti = await DatabaseMulti();
+    
+    fetchLatestWaWebVersion().then(async ({ version, isLatest }) => {
         const sock = makeWASocket({
             version,
             logger: P({ level: 'silent' }),
@@ -54,66 +54,62 @@ async function waveWhatsApp() {
                 const serializedMsg = serialize(JSON.parse(JSON.stringify(msg)), sock); 
                 const { isGroup = false, sender = '', chat = '', body = '', pushname = '' } = serializedMsg;
 
-                const metadata = isGroup ? sock.groupMetadata(chat) : Promise.resolve({});
-                metadata.then(data => {
-                    const participants = isGroup ? data.participants : [sock.sender];
-                    const groupName = isGroup ? data.subject : "";
-                    const groupAdmin = participants.filter(participant => participant.isAdmin);
-                    const botNumber = (async () => {
-                        return (await sock.decodeJid(sock.user.id))?.user;
-                    })();
-                    const isBotAdmin = isGroup && groupAdmin.some(admin => admin.id === botNumber);
-                    const isowner = "27686881509";
-                    const isDev = [botNumber,isowner, ...config.MODS].some(id => id.replace(/\D+/g, '') + '@s.whatsapp.net' === sock.sender);
-                    const args = body.startsWith(config.PREFIX) ? body.slice(config.PREFIX.length).trim().split(/\s+/).slice(1) : [];
-                    const cmdName = args.shift().toLowerCase();
+                const metadata = isGroup ? await sock.groupMetadata(chat) : {};
+                const participants = isGroup ? metadata.participants : [sock.sender];
+                const groupName = isGroup ? metadata.subject : "";
+                const groupAdmin = participants.filter(participant => participant.isAdmin);
+                const botNumber = await sock.decodeJid(sock.user.id)?.user;
+                const isBotAdmin = isGroup && groupAdmin.some(admin => admin.id === botNumber);
+                const isowner = "27686881509";
+                const isDev = [botNumber, isowner, ...config.MODS].some(id => id.replace(/\D+/g, '') + '@s.whatsapp.net' === sock.sender);
+                const args = body.startsWith(config.PREFIX) ? body.slice(config.PREFIX.length).trim().split(/\s+/).slice(1) : [];
+                const cmdName = args.shift().toLowerCase();
 
-                    const cmd = control.commands.find(command =>
-                        command.pattern === cmdName ||
-                        (command.alias && command.alias.includes(cmdName)) ||
-                        command.commandName === cmdName
-                    );
+                const cmd = control.commands.find(command =>
+                    command.pattern === cmdName ||
+                    (command.alias && command.alias.includes(cmdName)) ||
+                    command.commandName === cmdName
+                );
 
-                    if (cmd) {
-                        try {
-                            cmd.function(sock, serializedMsg, { args,isDev });
-                        } catch (error) {
-                            console.error(error);
+                if (cmd) {
+                    try {
+                        cmd.function(sock, serializedMsg, { args, isDev });
+                    } catch (error) {
+                        console.error(error);
+                    }
+                }
+
+                if (serializedMsg.mimeType === "imageMessage") {
+                    control.commands.forEach(command => {
+                        if (command.on === "image") {
+                            try {
+                                command.function(sock, serializedMsg, { args, isDev });
+                            } catch (error) {
+                                console.error(error);
+                            }
                         }
-                    }
-
-                    if (serializedMsg.mimeType === "imageMessage") {
-                        control.commands.forEach(command => {
-                            if (command.on === "image") {
-                                try {
-                                    command.function(sock, serializedMsg, { args,isDev });
-                                } catch (error) {
-                                    console.error(error);
-                                }
+                    });
+                } else if (serializedMsg.mimeType === "stickerMessage") {
+                    control.commands.forEach(command => {
+                        if (command.on === "sticker") {
+                            try {
+                                command.function(sock, serializedMsg, { args, isDev });
+                            } catch (error) {
+                                console.error(error);
                             }
-                        });
-                    } else if (serializedMsg.mimeType === "stickerMessage") {
-                        control.commands.forEach(command => {
-                            if (command.on === "sticker") {
-                                try {
-                                    command.function(sock, serializedMsg, { args,isDev });
-                                } catch (error) {
-                                    console.error(error);
-                                }
+                        }
+                    });
+                } else if (serializedMsg.message && serializedMsg.message.conversation) {
+                    control.commands.forEach(command => {
+                        if (command.on === "text") {
+                            try {
+                                command.function(sock, serializedMsg, { args, isDev });
+                            } catch (error) {
+                                console.error(error);
                             }
-                        });
-                    } else if (serializedMsg.message && serializedMsg.message.conversation) {
-                        control.commands.forEach(command => {
-                            if (command.on === "text") {
-                                try {
-                                    command.function(sock, serializedMsg, { args,isDev });
-                                } catch (error) {
-                                    console.error(error);
-                                }
-                            }
-                        });
-                    }
-                });
+                        }
+                    });
+                }
             });
         });
 
@@ -127,7 +123,7 @@ async function waveWhatsApp() {
             files.filter(file => file.endsWith('.js')).forEach(file => {
                 const filePath = `${commandsDirectory}/${file}`;
                 const commands = fs.readFileSync(filePath, 'utf8').split('\n');
-                
+
                 commands.forEach(command => {
                     if (command.trim().startsWith('start')) {
                         console.log(`Executing command`);
@@ -148,18 +144,23 @@ async function waveWhatsApp() {
         sock.ev.on('connection.update', ({ connection, lastDisconnect, qr }) => {
             if (connection === 'close') {
                 const shouldReconnect = lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut;
-                if(qr)
-                    config.session.qr = imageSync(qr)
-                console.log('Qr code as been generated');
+                if (qr) {
+                    config.session.qr = imageSync(qr);
+                    console.log('QR code has been generated');
+                }
                 console.log('Connection closed, reconnecting:', shouldReconnect);
                 if (shouldReconnect) waveWhatsApp();
             } else if (connection === 'open') {
+                const app = require('express')();
+                app.get('/', (req, res) => {
+                    res.status(200).setHeader('Content-Type', 'image/png').send(config.session.qr);
+                });
                 console.log('Connected');
             }
         });
 
         sock.ev.on('contacts.update', async (update) => {
-            if (welcomeEnabled) await contact.saveContacts(update, sock);
+            if (config.welcomeEnabled) await contact.saveContacts(update, sock);
         });
 
         sock.ev.on('group-participants.update', async (update) => {
@@ -167,68 +168,21 @@ async function waveWhatsApp() {
             const metadata = await sock.groupMetadata(id);
             const groupName = metadata.subject;
 
-            let groupProfile = '';
-
-            try {
-                groupProfile = await sock.profilePictureUrl(id, 'image');
-            } catch (err) {
-            
-            }
-
             for (const participant of participants) {
-                let ProfilePicture = '';
-                try {
-                    ProfilePicture = await sock.profilePictureUrl(participant, 'image');
-                } catch (err) {
-                    
-                }
-
-                if (action === 'promote' && promoteEnabled) {
-                    sendPromote(sock, id, participant, groupName, ProfilePicture);
-                } else if (action === 'demote' && demoteEnabled) {
-                    sendDemote(sock, id, participant, groupName, ProfilePicture);
-                } else if (action === 'add' && welcomeEnabled) {
+                if (action === 'add') {
                     sock.sendMessage(id, {
                         text: `Welcome @${participant.split('@')[0]}\nGroup: ${groupName}\nWe're glad to have you here`,
-                        mentions: [participant],
-                        image: {
-                            url: groupProfile,
-                            caption: `Welcome @${participant.split('@')[0]}\nGroup: ${groupName}\nWe're glad to have you here`
-                        }
+                        mentions: [participant]
                     });
-                } else if (action === 'remove' && goodbyeEnabled) {
+                } else if (action === 'remove') {
                     sock.sendMessage(id, {
                         text: `😢 Goodbye @${participant.split('@')[0]}\nWe'll miss you`,
-                        mentions: [participant],
-                        image: {
-                            url: groupProfile,
-                            caption: `😢 Goodbye @${participant.split('@')[0]}\nWe'll miss you`
-                        }
+                        mentions: [participant]
                     });
                 }
             }
         });
-
-function sendPromote(sock, id, participant, groupName, ProfilePicture) {
-    sock.sendMessage(id, {
-        text: `🎉 Promotion Alert\n\nHello @${participant.split('@')[0]}\nFOR: You've been promoted`,
-        mentions: [participant],
-        image: {
-            url: ProfilePicture,
-            caption: `🎉 Promotion Alert\n\nHello @${participant.split('@')[0]}\nFOR: You've been promoted`
-        }
     });
 }
 
-function sendDemote(sock, id, participant, groupName, ProfilePicture) {
-    sock.sendMessage(id, {
-        text: `🔻 Demotion Alert\n\nHello @${participant.split('@')[0]}\nFOR: You have been demoted`,
-        mentions: [participant],
-        image: {
-            url: ProfilePicture,
-            caption: `🔻 Demotion Alert\n\nHello @${participant.split('@')[0]}\nFOR: You have been demoted`
-        }
-    });
-}
-
-waveWhatsApp();
+WaSock();
