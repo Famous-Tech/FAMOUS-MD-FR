a74dcd178986b96e89f27708eebf3fd9446b8ae0
A7-T62 <109889758+Diegoson@users.noreply.github.com>
2024-07-05 21:50:28 +0200

    🐊

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
-const config = {
-  API_BASE_URL: "https://api.astrid.com",
-  mongodb: process.env.MONGODB_URL || 'mongodb+srv://z:z@cluster0.sy21r5d.mongodb.net/?retryWrites=true&w=majority',
-  prefix: process.env.PREFIX || '.',
-  session: process.env.SESSION || 'Diegoson',
-  
+require('dotenv').config(); 
+
+module.exports = {
+    API_BASE_URL: "https://api.astrid.com",
+    MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://z:z@cluster0.sy21r5d.mongodb.net/?retryWrites=true&w=majority',
+    PREFIX: process.env.PREFIX || '!',
+    SESSION: process.env.SESSION || 'session_key',
+    MODS: process.env.MODS ? JSON.parse(process.env.MODS) : ['27686881509@s.whatsapp.net']
 };
-
-module.exports = config;
-
