2311915061d7b02090dbe7d1053ecb912e31a07b
A7-T62 <109889758+Diegoson@users.noreply.github.com>
2024-07-05 07:36:02 +0200

    🐊

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const global = {
   API_BASE_URL: "https://api.astrid.com",
   mongodb: process.env.MONGODB_URL || '',
   prefix: process.env.PREFIX || '.',
+  session: process.env.SESSION|| 'Diegoson',
   
 };
 
