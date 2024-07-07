e47b33f32a2aed70e646cde46f66c31d1e1cbad3
A7-T62 <109889758+Diegoson@users.noreply.github.com>
2024-07-07 14:49:28 +0200

    🐊

--- contents/QuickDB/Schema.js
+++ contents/QuickDB/Schema.js
@@ -1,90 +1,93 @@
 const WASocket = require('@whiskeysockets/baileys');
-
 const Authentication = (sessionId, database, WASocket) => {
-    let credentials = null;
-    let keys = {};
+  let credentials = null;
+  let keys = {};
 
-    const getKeyMap = (type) => {
-        const keyMap = {
-            'pre-key': 'preKeys',
-            session: 'sessions',
-            'sender-key': 'senderKeys',
-            'app-state-sync-key': 'appStateSyncKeys',
-            'app-state-sync-version': 'appStateVersions',
-            'sender-key-memory': 'senderKeyMemory'
-        };
-        return keyMap[type];
+  database.getSession = async (sessionId) => {
+    try {
+      const sessionData = await database.collection('sessions').findOne({ _id: sessionId });
+      return sessionData;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+
+  const getKeyMap = (type) => {
+    const keyMap = {
+      'pre-key': 'preKeys',
+      session: 'sessions',
+      'sender-key': 'senderKeys',
+      'app-state-sync-key': 'appStateSyncKeys',
+      'app-state-sync-version': 'appStateVersions',
+      'sender-key-memory': 'senderKeyMemory'
+    };
+    return keyMap[type];
+  };
+
+  const DatabaseMulti = async () => {
+    if (!database) {
+      throw new Error('Database object is undefined.');
+    }
+    if (typeof database.getSession !== 'function') {
+      throw new Error('getSession method is not defined on the database object.');
+    }
+    console.log('Database object:', database);
+    console.log('Database getSession method:', database.getSession);
+    console.log('Database setSession method:', database.setSession);
+    console.log('Database deleteSession method:', database.deleteSession);
+
+    const sessionData = await database.getSession(sessionId);
+    if (sessionData && sessionData.session) {
+      const { credentials: storedCredentials, keys: storedKeys } = JSON.parse(sessionData.session, WASocket.BufferJSON.reviver);
+      credentials = storedCredentials;
+      keys = storedKeys;
+    } else {
+      credentials = initCredentials();
+      keys = {};
+    }
+
+    const saveState = async () => {
+      const session = JSON.stringify({ credentials, keys }, WASocket.BufferJSON.replacer, 2);
+      await database.setSession(sessionId, { session });
     };
 
-    const DatabaseMulti = async () => {
-        if (!database) {
-            throw new Error('Database object is undefined.');
-        }
-
-        if (typeof database.getSession !== 'function') {
-            throw new Error('getSession method is not defined on the database object.');
-        }
-
-        console.log('Database object:', database);
-        console.log('Database getSession method:', database.getSession);
-        console.log('Database setSession method:', database.setSession);
-        console.log('Database deleteSession method:', database.deleteSession);
-
-        const sessionData = await database.getSession(sessionId);
-
-        if (sessionData && sessionData.session) {
-            const { credentials: storedCredentials, keys: storedKeys } = JSON.parse(sessionData.session, WASocket.BufferJSON.reviver);
-            credentials = storedCredentials;
-            keys = storedKeys;
-        } else {
-            credentials = initCredentials();
-            keys = {};
-        }
-
-        const saveState = async () => {
-            const session = JSON.stringify({ credentials, keys }, WASocket.BufferJSON.replacer, 2);
-            await database.setSession(sessionId, { session });
-        };
-
-        const clearState = async () => {
-            await database.deleteSession(sessionId);
-            credentials = null;
-            keys = {};
-        };
-
-        const getKeys = (type, ids) => {
-            const key = getKeyMap(type);
-            return keys[key] ? ids.reduce((result, id) => {
-                if (keys[key][id]) result[id] = keys[key][id];
-                return result;
-            }, {}) : {};
-        };
-
-        const setKeys = (type, data) => {
-            const key = getKeyMap(type);
-            keys[key] = { ...(keys[key] || {}), ...data };
-            saveState();
-        };
-
-        return {
-            state: {
-                credentials,
-                keys: {
-                    get: getKeys,
-                    set: setKeys,
-                },
-            },
-            saveState,
-            clearState,
-        };
+    const clearState = async () => {
+      await database.deleteSession(sessionId);
+      credentials = null;
+      keys = {};
     };
 
-    const initCredentials = () => ({ username: '', token: '' });
+    const getKeys = (type, ids) => {
+      const key = getKeyMap(type);
+      return keys[key] ? ids.reduce((result, id) => {
+        if (keys[key][id]) result[id] = keys[key][id];
+        return result;
+      }, {}) : {};
+    };
+
+    const setKeys = (type, data) => {
+      const key = getKeyMap(type);
+      keys[key] = { ...(keys[key] || {}), ...data };
+      saveState();
+    };
 
     return {
-        DatabaseMulti,
+      state: {
+        credentials,
+        keys: {
+          get: getKeys,
+          set: setKeys
+        }
+      },
+      saveState,
+      clearState
     };
+  };
+
+  const initCredentials = () => ({ username: '', token: '' });
+
+  return { DatabaseMulti };
 };
 
 module.exports = Authentication;
-
