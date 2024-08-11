64003fb22d729a54708d1134675d49322b5df40b
naxor <109889758+Diegoson@users.noreply.github.com>
2024-08-11 10:24:19 +0200

    🤺

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ module.exports = {
     API_BASE_URL: "https://api.astrid.com",
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://z:z@cluster0.sy21r5d.mongodb.net/?retryWrites=true&w=majority',
     PREFIX: process.env.PREFIX || '!',
-    session: process.env.SESSION || 'session_key',
+    PER_ANTI: process.env.PER_ANTI || 'true', // false
+    SESSION: process.env.SESSION || 'session_key',
     MODS: process.env.MODS ? JSON.parse(process.env.MODS) : ['27686881509@s.whatsapp.net']
 };
