09f5c343299b0fa18a588ebbe1544f2556aec97c
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-08-11 13:46:14 +0200

    🤺

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ module.exports = {
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://z:z@cluster0.sy21r5d.mongodb.net/?retryWrites=true&w=majority',
     PREFIX: process.env.PREFIX || '!',
     PER_ANTI: process.env.PER_ANTI || 'true', // false
-    SESSION: process.env.SESSION || 'session_key',
+    SESSION_ID: process.env.SESSION_ID || 'session_key',
     MODS: process.env.MODS ? JSON.parse(process.env.MODS) : ['27686881509@s.whatsapp.net']
 };
