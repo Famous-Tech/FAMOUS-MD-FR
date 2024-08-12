5e3eb6ca27594b01b4096cd70b4239e78a9cb6f8
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-08-12 20:14:14 +0200

    🤺

--- commands/media.js
+++ commands/media.js
@@ -39,4 +39,48 @@ Meta({
     await sock.sendMessage(from, { sticker: st_Data }, { quoted: message[0] });
   }
 });
+
+let polls = {};
+
+Meta({
+  command: 'vote',
+  category: 'group',
+  handler: async (sock, message, matched) => {
+    const { key, from, isGroup, message: msg } = message[0];
+    const input = matched.input.trim().split(' ').slice(1).join(' ');
+
+    if (!isGroup) {
+      return await sock.sendMessage(from, {
+        text: 'This command can only be used in a group' }, { quoted: message[0] });
+    }
+
+    if (!input) {
+      return await sock.sendMessage(from, { 
+        text: 'Usage: !vote <topic> to create a poll, !vote <option> to vote' }, { quoted: message[0] });
+    }
+
+    if (!polls[from]) {
+      polls[from] = {
+        topic: input,
+        options: {},
+        voters: []
+      };
+      await sock.sendMessage(from, { text: `Poll created: ${input}\n\nTo vote, use: !vote <option>` }, { quoted: message[0] });
+    } else {
+      const poll = polls[from];
+      if (poll.voters.includes(key.participant)) {
+        return await sock.sendMessage(from, { text: 'You have already voted in this poll' }, { quoted: message[0] });
+      }
+
+      poll.options[input] = (poll.options[input] || 0) + 1;
+      poll.voters.push(key.participant);
+      let result = `*Poll:* ${poll.topic}\n\n`;
+      for (let option in poll.options) {
+        result += `*${option}:* ${poll.options[option]} votes\n`;
+      }
+      await sock.sendMessage(from, { text: result }, { quoted: message[0] });
+    }
+  }
+});
+        
       
