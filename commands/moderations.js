f264c52516e03147b1342ee3b1c298d0909198fe
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-08-25 17:18:24 +0200

    🤺

--- commands/moderations.js
+++ commands/moderations.js
@@ -4,16 +4,16 @@ const { setTimeout } = require('timers/promises');
 Meta({
   command: 'ban',
   category: 'moderation',
-  handler: async (sock, message, matched) => {
-    const { from, key, mentionedJid } = message[0];
+  handler: async (sock, message, args) => {
+    const { from, key, mentionedJid } = message;
     const [_, duration] = matched;
-    const target = mentionedJid[0];
+    const target = mentionedJid;
 
     if (!target || !duration) {
-      return await sock.sendMessage(from, { text: 'Please mention a user and specify a ban duration, e.g., @naxor_ser' }, { quoted: message[0] });
+      return await sock.sendMessage(from, { text: 'Please mention a user and specify a ban duration, e.g., @naxor_ser' }, { quoted: message });
     }
     await sock.groupRemove(from, [target]);
-    await sock.sendMessage(from, { text: `@${target.split('@')[0]} has been banned for ${duration}.`, mentions: [target] }, { quoted: message[0] });
+    await sock.sendMessage(from, { text: `@${target.split('@')[0]} has been banned for ${duration}.`, mentions: [target] }, { quoted: message });
     const milliseconds = parseDuration(duration);
     setTimeout(milliseconds).then(async () => {
       await sock.groupAdd(from, [target]);
@@ -31,6 +31,4 @@ function parseDuration(duration) {
   const unit = duration.slice(-1);
   const time = parseInt(duration.slice(0, -1));
   return time * timeUnits[unit];
-  }
-                                  
-commands.push('ban')
+  }                            
