8509d13e354d0ec4cc01299d2fb27de676fc81c7
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-08-25 17:00:32 +0200

    🤺

--- commands/YouTube.js
+++ commands/YouTube.js
@@ -4,20 +4,19 @@ const { YT_SEARCH } = require('../lib/YOUTUBE.js');
 Meta({
   command: 'yts',
   category: 'youtube',
-  handler: async (sock, message, matched) => {
-    const { from } = message[0];
-    const query = matched.slice(8).trim();
-    if (!query) {
-      return await sock.sendMessage(from, { text: 'Usage: !youtube [search query]' }, { quoted: message[0] });
+  handler: async (sock, message, args) => {
+    const { from } = message;
+      if (!args) {
+      return await sock.sendMessage(from, { text: 'Usage: !youtube [search query]' }, { quoted: message });
     }
     try {
-      const results = await YT_SEARCH(query);
+      const results = await YT_SEARCH(args);
       if (results.length === 0) {
-        return await sock.sendMessage(from, { text: 'No results found' }, { quoted: message[0] });
+        return await sock.sendMessage(from, { text: 'No results found' }, { quoted: message });
       } const res = results.map((video, index) => 
         `${index + 1}. ${video.title}\n${video.url}\n${video.description}\n*Duration*: ${video.duration}\n`
       ).join('\n');
-      await sock.sendMessage(from, { text: `**YouTube Search Results:**\n\n${response}` }, { quoted: message[0] });
+      await sock.sendMessage(from, { text: `**YouTube Search:**\n\n${res}` }, { quoted: message });
     } catch (error) {
         }
   }
