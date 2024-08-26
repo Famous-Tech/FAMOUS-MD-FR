2bada705a7663ade88fe204822dc877473c8245c
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-08-26 19:05:42 +0200

    🤺

--- commands/menu.js
+++ commands/menu.js
@@ -1,40 +1,67 @@
-const { commands, Meta, Unicode } = require('../lib/'); 
-const config = require('../config'); 
+const { commands, Meta, Unicode } = require('../lib/');
+const config = require('../config');
 
 Meta({
     command: 'menu',
     category: 'mics',
     handler: async (sock, args, message) => {
         const { from } = message;
+      
+        const text = args.join(' ');
+        if (text.startsWith(`${config.PREFIX}list`)) {
+            let list_str = '𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 𝐋𝐢𝐬𝐭:\n';
+            commands.forEach(cmd => {
+                list_str += `- ${Unicode(cmd.command)}\n`;
+            });
+
+            await sock.sendMessage(from, { text: list_str });
+            return;
+        }
+
+        const [prefix, filename, command_name] = text.split(' ');
+        if (prefix === config.PREFIX && filename && command_name) {
+            const cmd = commands.find(cmd => cmd.command === command_name);
+            if (cmd) {
+                const { category, description } = cmd;
+                const details_str = `✦ 𝐂𝐨𝐦𝐦𝐚𝐧𝐝: ${Unicode(command_name)}\n` +
+                                    `✦ 𝐅𝐢𝐥𝐞𝐧𝐚𝐦𝐞: ${filename}\n` +
+                                    `✦ 𝐂𝐚𝐭𝐞𝐠𝐨𝐫𝐲: ${Unicode(category)}\n` +
+                                    `✦ 𝐃𝐞𝐬𝐜𝐫𝐢𝐩𝐭𝐢𝐨𝐧: ${Unicode(description)}`;
+
+                await sock.sendMessage(from, { text: details_str });
+                return;
+            } else {
+                await sock.sendMessage(from, { text: '3ccr' });
+                return;
+            }
+        }
 
         const cmd_str = commands.reduce((acc, cmd) => {
-            const { category, command, filename, description } = cmd;
+            const { category, command, description } = cmd;
             if (!acc[category]) {
                 acc[category] = [];
             }
-            acc[category].push({ command, filename, description });
+            acc[category].push({ command, description });
             return acc;
         }, {});
 
-        
-        let menu_str = '╭───╼〔 𝐗-𝐀𝐒𝐓𝐑𝐀𝐋 𝐁𝐎𝐓 〕
-┃ ✦ ${Unicode('Owner')} : ${config.OWNER}
-┃ ✦ ${Unicode('User')} : 
-┃ ✦ ${Unicode('Mode')} : ${config.MODE}
-┃ ✦ ${Unicode('Version')} : ${config.VERSION}
-╰──────────╼\n';
-        
+        let menu_str = `╭───╼〔 𝐗-𝐀𝐒𝐓𝐑𝐀𝐋 𝐁𝐎𝐓 〕\n`;
+        menu_str += `┃ ✦ ${Unicode('Owner')} : ${config.OWNER}\n`;
+        menu_str += `┃ ✦ ${Unicode('User')} : \n`;
+        menu_str += `┃ ✦ ${Unicode('Mode')} : ${config.MODE}\n`;
+        menu_str += `┃ ✦ ${Unicode('Version')} : ${config.VERSION}\n`;
+        menu_str += `╰──────────╼\n`;
+
         Object.keys(cmd_str).forEach(category => {
             menu_str += `╭───╼〔 ${Unicode(category.toUpperCase())} 〕\n`;
             cmd_str[category].forEach(cmd => {
-                const { command, filename, description } = cmd;
+                const { command } = cmd;
                 menu_str += `┃ ∘ ${Unicode(command)}\n`;
             });
+            menu_str += `╰──────────╼\n`;
         });
 
-        menu_str += `╰──────────╼`;
-        
         await sock.sendMessage(from, { text: menu_str });
     }
 });
-  
+               
