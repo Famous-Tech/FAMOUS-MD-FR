67668939dac72d7799eb3b5182dc938d4184664d
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-08-31 18:09:49 +0200

    🤺

--- commands/owner.js
+++ commands/owner.js
@@ -1,5 +1,6 @@
 const { commands, Meta } = require('../lib');
 const fs = require('fs');
+const config = require('../config');
 const path = require('path');
 const { MessageType } = require('@whiskeysockets/baileys');
 
@@ -7,10 +8,10 @@ Meta({
   command: 'exe',
   category: 'owner',
   filename: __filename,
-  handler: async (sock, message, args, author) => {
+  handler: async (sock, message, args, author, languages) => {
     const { from } = message;
     if(!author) {
-      return sock.sendMessage(from, { text: '*This command is for ny owner*' }, MessageType.text);
+      return sock.sendMessage(from, { text: languages[config.LANGUAGE].OWNER_MSG}, MessageType.text);
     const [Ext] = args;
     if (!Ext) {
       return sock.sendMessage(from, { text: 'Please provide a filename\n naxor.js' }, { quoted: message });
@@ -28,5 +29,26 @@ Meta({
     } catch (error) {
       console.error(error);
           }
+  },
+});
+
+Meta({
+  command: 'lang',
+  category: 'owner',
+  filename: __filename,
+  handler: async (sock, message, args, author, languages) => {
+    const { from } = message;
+if(!author){
+return sock.sendMessage(from,{text: languages[config.LANGUAGE].OWNER_MSG});
+    if (!args.length) {
+      await sock.sendMessage(from, { text: `languages: en, sn, ml, zu` });
+      return;
+    }
+    const newLang = args[0].toLowerCase();
+    if (!['en', 'sn', 'ml', 'zu'].includes(newLang)) {
+      await sock.sendMessage(from, { text: 'Please choose from: en, sn, ml, zu' });
+      return;
+    } config.LANGUAGE = newLang;
+    await sock.sendMessage(from, { text: `Language changed:${newLang}` });
   },
 });
