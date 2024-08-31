424fcdad00898e2dac8696891c77c6b5269512aa
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-08-31 17:50:48 +0200

    🤺

---
+++ data_store/languages.js
@@ -0,0 +1,27 @@
+const languages = {
+  en: {
+    OWNER_MSG: 'This command is for my owner only',
+    GROUP_MSG: 'This command is for group only',
+    DOWNLOAD_MSG: 'Provide a search query',
+    URL_MSG: 'Please provide a YouTube URL',
+  },
+  sn: {
+    OWNER_MSG: 'Iri rairo ndeyemuridzi wangu chete',
+    GROUP_MSG: 'Iri rairo ndeyeboka chete',
+    DOWNLOAD_MSG: 'Ipa chirevo chekutsvaga',
+    URL_MSG: 'Ndokumbira upe URL yeYouTube',
+  },
+  ml: {
+    OWNER_MSG: 'ഈ കമാൻഡ് എന്റെ ഉടമക്ക് മാത്രമാണ്',
+    GROUP_MSG: 'ഈ കമാൻഡ് ഗ്രൂപ്പിനായി മാത്രമാണ്',
+    DOWNLOAD_MSG: 'തിരച്ചിൽ ചോദ്യാവലി നൽകുക',
+    URL_MSG: 'ദയവായി ഒരു YouTube URL നൽകുക',
+  },
+  zu: {
+    OWNER_MSG: 'Lo myalo ungowomnikazi wami kuphela',
+    GROUP_MSG: 'Lo myalo ungoweqembu kuphela',
+    DOWNLOAD_MSG: 'Nikeza umbuzo wokusesha',
+    URL_MSG: 'Sicela unikeze i-URL ye-YouTube',
+  },
+};
+module.exports = {languages};
