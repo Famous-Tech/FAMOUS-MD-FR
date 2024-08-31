f96f3c01dbadb70cb019c4dda301a27021be7de7
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-08-31 14:02:31 +0200

    🤺

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ module.exports = {
     API_BASE_URL: 'https://api.astrid.com',
     VERSION: require('./package.json').version,
     OWNER: process.env.OWNER ||'Its You😂',
+    DEEPAI_KEY: '7f831cb9-aa30-4270-bfa6-0a5d4d03afac',
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://z:z@cluster0.sy21r5d.mongodb.net/?retryWrites=true&w=majority',
     PREFIX: process.env.PREFIX || '!',
     PER_ANTI: process.env.PER_ANTI || 'true', // false
