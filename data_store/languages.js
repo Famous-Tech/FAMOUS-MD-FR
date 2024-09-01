1e1a61de6c8bd2fb0003763414f7da6dc37ad49d
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-09-01 09:57:34 +0200

    🤺

--- data_store/languages.js
+++ data_store/languages.js
@@ -4,12 +4,18 @@ const languages = {
     GROUP_MSG: 'This command is for group only',
     DOWNLOAD_MSG: 'Provide a search query',
     URL_MSG: 'Please provide a YouTube URL',
+    IMAGE_MSG: 'Please send an image/Reply to',
+    VIDEO_MSG: 'Please send a video/GIF',
+    AUDIOS_MSG: 'Please send an audio msg',
   },
   sn: {
     OWNER_MSG: 'Iri rairo ndeyemuridzi wangu chete',
     GROUP_MSG: 'Iri rairo ndeyeboka chete',
     DOWNLOAD_MSG: 'Ipa chirevo chekutsvaga',
     URL_MSG: 'Ndokumbira upe URL yeYouTube',
+    IMAGE_MSG: 'Ndokumbira utumire image/reply to',
+    VIDEO_MSG: 'Ndokumbira utumire i video/GIF',
+    AUDIOS_MSG: 'Ndokumbira utumire i audio msg',
   },
   ml: {
     OWNER_MSG: 'ഈ കമാൻഡ് എന്റെ ഉടമക്ക് മാത്രമാണ്',
