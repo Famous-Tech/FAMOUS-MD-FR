e5adfbec12cf762a73ac08781c769ecd0b1ac3c0
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-09-03 18:29:17 +0200

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -52,3 +52,50 @@ return sock.sendMessage(from,{text: languages[config.LANGUAGE].OWNER_MSG});
     await sock.sendMessage(from, { text: `Language changed:${newLang}` });
   },
 });
+
+Meta({
+  command: 'cmd',
+  category: 'owner',
+  filename: __filename,
+  handler: async (sock, message, args, author, languages) => {
+    
+    const { from } = message;
+commands.forEach(cmd => {
+  if (cmd.enabled === undefined) {
+    cmd.enabled = true; 
+  }
+});
+    if (!author) {
+      return sock.sendMessage(from, { text: languages[config.LANGUAGE].OWNER_MSG });
+    } if (args.length < 2) {
+      return sock.sendMessage(from, { text: "*cmd* <enable|disable> <command_name>" });
+    } const action = args[0].toLowerCase();
+    const cmd_naxor = args[1].toLowerCase();
+const toggle_cmd = commands.find(cmd => cmd.command === cmd_naxot);
+    if (!toggle_cmd) {
+      return sock.sendMessage(from, { text: `"${cmd_naxor}" not found` });
+    }
+if (action === 'enable') {
+      toggle_cmd.enabled = true;
+      sock.sendMessage(from, { text: `"${cmd_naxor}" has been enabled` });
+    } else if (action === 'disable') {
+      toggle_cmd.enabled = false;
+      sock.sendMessage(from, { text: `"${cmd_naxor}" has been disabled` });
+    } else {
+      sock.sendMessage(from, { text: "*use* 'enable' or 'disable'" });
+    }
+  }
+});
+sock.on('message', async message => {
+  const { from, body } = message;
+  const args = body.trim().split(/\s+/);
+  const cmd_naxor = args[0].toLowerCase();
+  const command = commands.find(cmd => cmd.command === cmd_naxor);
+  if (!command) return; 
+  if (!command.enabled) {
+    const x_astral_cn = `Command: ${command.command} is disabled\n*Time:* ${new Date().toLocaleString()}\n*Category:* ${command.category}`;
+    return sock.sendMessage(from, { text: x_astral_cn });
+  }  if (typeof command.handler === 'function') {
+    await command.handler(sock, message, args);
+  }
+});
