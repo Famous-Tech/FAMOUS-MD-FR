a84492c55a342b03f394bf112205af907a0e9a4a
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-09-07 16:34:13 +0200

    🤺

--- commands/mics.js
+++ commands/mics.js
@@ -11,3 +11,18 @@ Meta({
         await sock.sendMessage(from, { text: `"${quote.content}" - *${quote.author}*` });
     }
 });
+
+Meta({
+    command: 'translate',
+    category: 'utilities',
+    handler: async (sock, message, args) => {
+        const { from } = message;
+        const [targetLang, ...text] = args;
+        const textToTranslate = text.join(' ');
+        if (!targetLang || !textToTranslate) {
+            return sock.sendMessage(from, { text: 'use: *translate* <language-code> <text>' });
+        }    const res_stz = await axios.get(`https://api.mymemory.translated.net/get?q=${encodeURIComponent(textToTranslate)}&langpair=en|${targetLang}`);
+        const naxors = res_stz.data.responseData.translatedText;
+        await sock.sendMessage(from, { text: naxors });
+    }
+});
