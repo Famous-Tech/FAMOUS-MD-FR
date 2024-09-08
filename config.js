230cb4f4664b0611744def8f83282da53566f44f
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-09-08 15:48:27 +0200

    🤺

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ module.exports = {
     DEEPAI_KEY: '7f831cb9-aa30-4270-bfa6-0a5d4d03afac',
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://z:z@cluster0.sy21r5d.mongodb.net/?retryWrites=true&w=majority',
     PREFIX: process.env.PREFIX || '!',
+    antilink: {},
     PER_ANTI: process.env.PER_ANTI || 'true', // false
     SESSION_ID: process.env.SESSION_ID || 'session_key',
     BRAINSHOP_PRIVATE: process.env.BRAINSHOP_PRIVATE || 'false', //true
