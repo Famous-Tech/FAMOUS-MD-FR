21915444b7153e0fc75bd6e827c9f9b543e5f0ca
ᴺᴀxᴼʀ <109889758+Diegoson@users.noreply.github.com>
2024-09-08 15:51:43 +0200

    🤺

--- config.js
+++ config.js
@@ -1,14 +1,12 @@
 require('dotenv').config(); 
 
 module.exports = {
-    API_BASE_URL: 'https://api.astrid.com',
     VERSION: require('./package.json').version,
     OWNER: process.env.OWNER ||'Its You😂',
     DEEPAI_KEY: '7f831cb9-aa30-4270-bfa6-0a5d4d03afac',
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://z:z@cluster0.sy21r5d.mongodb.net/?retryWrites=true&w=majority',
     PREFIX: process.env.PREFIX || '!',
     antilink: {},
-    PER_ANTI: process.env.PER_ANTI || 'true', // false
     SESSION_ID: process.env.SESSION_ID || 'session_key',
     BRAINSHOP_PRIVATE: process.env.BRAINSHOP_PRIVATE || 'false', //true
     MODE: process.env.MODE || 'private', // public
