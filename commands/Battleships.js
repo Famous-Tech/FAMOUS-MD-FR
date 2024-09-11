03abba59fe93cb3c012f30b4ded89a7bff937e06
FAMOUS-TECH <167099698+Famous-Tech@users.noreply.github.com>
2024-09-11 19:33:32 -0400

    V1💫

--- commands/Battleships.js
+++ commands/Battleships.js
@@ -4,35 +4,35 @@ const instances = {};
 
 Meta({
   command: 'battleship',
-  category: 'games',
+  category: 'jeux',
   filename: 'Battleships.js',
   handler: async(sock, args, message) => {
   const { from } = message;
   
   let game = instances[from];
   if (!game) {
-    game = new Battleship('Player 1', true);
+    game = new Battleship('Joueur 1', true);
     instances[from] = game;
-    sock.sendMessage(from, { text: `Battleship game started\n\nPlayer 1 vs XAstarl\n\n${game.renderBoard(game.boards['player1'])}` });
+    sock.sendMessage(from, { text: `Partie de Bataille Navale démarrée\n\nJoueur 1 vs XAstarl\n\n${game.renderBoard(game.boards['player1'])}` });
   } else if (game.isGameOver) {
-    sock.sendMessage(from, { text: `Game over Please start a new game` });
+    sock.sendMessage(from, { text: `Partie terminée. Veuillez démarrer une nouvelle partie` });
   } else {
     if (game.x_astral) {
       if (game.getCurrentPlayer().name === 'Bot') {
         const [botRow, botCol] = game.botMove();
         const hit = game.attack('player2', botRow, botCol);
-        sock.sendMessage(from, { text: `Bot attacks at (${botRow + 1}, ${botCol + 1}) and its a ${hit ? 'hit' : 'miss'}\n\n${game.renderBoard(game.boards['player1'])}` });
+        sock.sendMessage(from, { text: `Bot attaque à (${botRow + 1}, ${botCol + 1}) et c'est un ${hit ? 'coup' : 'raté'}\n\n${game.renderBoard(game.boards['player1'])}` });
         game.nextPlayer();
       }
     }
 
     const [row, col] = args.map(n => parseInt(n, 10) - 1);
     if (!game.isValidMove(row, col)) {
-      sock.sendMessage(from, { text: `Invalid move: Please choose again` });
+      sock.sendMessage(from, { text: `Mouvement invalide: Veuillez choisir à nouveau` });
       return;
     }
     const hit = game.attack('player1', row, col);
-    sock.sendMessage(from, { text: `Player attacks at (${row + 1} ${col + 1}) and its a ${hit ? 'hit' : 'miss'}\n\n${game.renderBoard(game.boards['player2'])}` });
+    sock.sendMessage(from, { text: `Joueur attaque à (${row + 1} ${col + 1}) et c'est un ${hit ? 'coup' : 'raté'}\n\n${game.renderBoard(game.boards['player2'])}` });
     game.nextPlayer();
   }
 };
