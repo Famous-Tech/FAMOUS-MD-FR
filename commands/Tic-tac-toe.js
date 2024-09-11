7e7f154a33ec0b3df08d18379d6a49ec66bf9011
FAMOUS-TECH <167099698+Famous-Tech@users.noreply.github.com>
2024-09-11 19:23:40 -0400

    V1💫

--- commands/Tic-tac-toe.js
+++ commands/Tic-tac-toe.js
@@ -3,13 +3,13 @@ const X_TicTacToe = require('../data_store/tic-tac-toe.d.js');
 
 Meta({
     command: 'tictac',
-    category: 'games',
+    category: 'jeux',
     filename: 'tic-tac-toe.js',
     handler: async (sock, message, mentionedJid, author, args) => {
         const { from } = message;
 
         if (args.length < 1 || mentionedJid.length < 1) {
-            await sock.sendMessage(from, { text: 'Please mention a user to challenge for Tic-Tac-Toe' });
+            await sock.sendMessage(from, { text: 'Veuillez mentionner un utilisateur à défier au Tic-Tac-Toe' });
             return;
         }
         
@@ -20,7 +20,7 @@ Meta({
         if (!game) {
             game = new X_TicTacToe(playerX, playerO);
             X_TicTacToe.saveGame([...X_TicTacToe.loadGame(), game]);
-            await sock.sendMessage(from, { text: `Game created: Waiting for @${playerO.split('@')[0]}`, mentions: [playerO] });
+            await sock.sendMessage(from, { text: `Partie créée: En attente de @${playerO.split('@')[0]}`, mentions: [playerO] });
 
             sock.ev.on('messages.upsert', async ({ messages }) => {
                 const msg = messages[0];
@@ -34,7 +34,7 @@ Meta({
                 }
             });
         } else {
-            await sock.sendMessage(from, { text: 'You already have an ongoing game' });
+            await sock.sendMessage(from, { text: 'Vous avez déjà une partie en cours' });
         }
 
         sock.ev.on('messages.upsert', async ({ messages }) => {
@@ -53,7 +53,7 @@ Meta({
                         await sock.sendMessage(from, { text: result.message });
                     }
                 } else {
-                    await sock.sendMessage(from, { text: 'Invalid move: Please choose a number between 1 and 9' });
+                    await sock.sendMessage(from, { text: 'Mouvement invalide: Veuillez choisir un nombre entre 1 et 9' });
                 }
             }
 
@@ -61,7 +61,7 @@ Meta({
             if (!game.gameOver && Date.now() - (game.lastMoveTime || 0) > TIMEOUT) {
                 game.gameOver = true;
                 X_TicTacToe.addPoints(game.currentTurn === playerX ? playerO : playerX, 1);
-                await sock.sendMessage(from, { text: `Game over🙃 @${game.currentTurn.split('@')[0]} took too long. @${(game.currentTurn === playerX ? playerO : playerX).split('@')[0]} wins.`, mentions: [playerX, playerO] });
+                await sock.sendMessage(from, { text: `Partie terminée🙃 @${game.currentTurn.split('@')[0]} a pris trop de temps. @${(game.currentTurn === playerX ? playerO : playerX).split('@')[0]} gagne.`, mentions: [playerX, playerO] });
                 X_TicTacToe.updateGame(game);
             }
         });
