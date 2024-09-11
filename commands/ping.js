9c317a9fef0e466381936a5b5269c7de5c89bfaf
FAMOUS-TECH <167099698+Famous-Tech@users.noreply.github.com>
2024-09-11 16:05:40 -0400

    V1💫

--- commands/ping.js
+++ commands/ping.js
@@ -6,12 +6,12 @@ Meta({
     handler: async (sock, args, message) => {
         const { from } = message;
         const start = new Date().getTime();
-        const edited = await sock.sendMessage(from, { text: '🏓 Pinging...' }, { quoted: message });
+        const edited = await sock.sendMessage(from, { text: '_*Calcul de la vitesse...*_' }, { quoted: message });
 
         const end = new Date().getTime();
         const TripTime = end - start;
         await sock.sendMessage(from, {
-            text: `🏓 Pong!: \`${TripTime} ms`,
+            text: `Vitesss : \`${TripTime} ms`,
             edit: edited.key 
         });
     }
