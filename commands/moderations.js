a5ad86daf9e2f105cd5dd5f6fd0ef66490203d7b
FAMOUS-TECH <167099698+Famous-Tech@users.noreply.github.com>
2024-09-11 16:15:42 -0400

    V1💫

--- commands/moderations.js
+++ commands/moderations.js
@@ -3,24 +3,25 @@ const { setTimeout } = require('timers/promises');
 
 Meta({
   command: 'ban',
-  category: 'moderation',
+  category: 'modération',
   handler: async (sock, message, args) => {
     const { from, key, mentionedJid } = message;
     const [_, duration] = matched;
     const target = mentionedJid;
 
     if (!target || !duration) {
-      return await sock.sendMessage(from, { text: 'Please mention a user and specify a ban duration, e.g., @naxor_ser' }, { quoted: message });
+      return await sock.sendMessage(from, { text: 'Veuillez mentionner un utilisateur et spécifier une durée de bannissement, par exemple, @Famous-Tech }, { quoted: message });
     }
     await sock.groupRemove(from, [target]);
-    await sock.sendMessage(from, { text: `@${target.split('@')[0]} has been banned for ${duration}.`, mentions: [target] }, { quoted: message });
+    await sock.sendMessage(from, { text: `@${target.split('@')[0]} a été banni pendant ${duration}.`, mentions: [target] }, { quoted: message });
     const milliseconds = parseDuration(duration);
     setTimeout(milliseconds).then(async () => {
       await sock.groupAdd(from, [target]);
-      await sock.sendMessage(from, { text: `@${target.split('@')[0]} has been unbanned and added back to the group`, mentions: [target] });
+      await sock.sendMessage(from, { text: `@${target.split('@')[0]} a été débanni et ajouté de nouveau au groupe`, mentions: [target] });
     });
   }
 });
+
 function parseDuration(duration) {
   const timeUnits = {
     's': 1000,
@@ -31,4 +32,4 @@ function parseDuration(duration) {
   const unit = duration.slice(-1);
   const time = parseInt(duration.slice(0, -1));
   return time * timeUnits[unit];
-  }                            
+                                             }
